64b64693f1d964d19233538749c217f30d2d5d44
Jonatandb <jonatandb@gmail.com>
2024-09-04 21:48:52 -0300

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RestaurantPOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
