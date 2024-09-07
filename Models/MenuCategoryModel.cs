50ef1bfbe0e50fec652779063a856330e07a8827
Jonatandb <jonatandb@gmail.com>
2024-09-06 23:19:05 -0300

    MenuCategoryModel created to keep track of selected category

---
+++ Models/MenuCategoryModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RestaurantPOS.Data;
+
+namespace RestaurantPOS.Models
+{
+    public partial class MenuCategoryModel : ObservableObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+
+        [ObservableProperty]
+        private bool _isSelected;
+
+        public static MenuCategoryModel FromEntity(MenuCategory entity) => new()
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            Icon = entity.Icon
+        };
+    }
+}
