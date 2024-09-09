7300ed4c0b6a802036fa7fcf48ec00d1430a4d1a
Jonatandb <jonatandb@gmail.com>
2024-09-08 22:59:13 -0300

    Main window minimum height and minimum width setted

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,5 +16,12 @@ namespace RestaurantPOS
                 .GetResult();
         }
 
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.MinimumHeight = 760;
+            window.MinimumWidth = 1280;
+            return window;
+        }
     }
 }
