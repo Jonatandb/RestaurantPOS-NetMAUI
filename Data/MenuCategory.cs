b95e268199a700269ce2a5af590dd037b041e5ce
Jonatandb <jonatandb@gmail.com>
2024-09-09 21:04:23 -0300

    Formatting applied

--- Data/MenuCategory.cs
+++ Data/MenuCategory.cs
@@ -7,6 +7,6 @@ namespace RestaurantPOS.Data
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Icon{ get; set; }
+        public string Icon { get; set; }
     }
 }
