190f724d7a787a9d0f252a541c1675162b55959e
Jonatandb <jonatandb@gmail.com>
2024-09-14 14:06:58 -0300

    Fix to item price showed incorrectly during edition. Solution provided by Phorm.ai: https://www.phorm.ai/query?projectId=1dbb5345-55f1-4ce7-96be-a608c6520e8d&threadId=daa03eab-a9df-49a4-93fd-b31d33230dae

---
+++ Convertes/DecimalToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace RestaurantPOS.Converters
+{
+    public class DecimalToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal decimalValue)
+            {
+                return decimalValue.ToString("0.##", culture);
+            }
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue && decimal.TryParse(stringValue, NumberStyles.Any, culture, out decimal result))
+            {
+                return result;
+            }
+            return 0m;
+        }
+    }
+}
